8182c1c1db47e40377794ad492f9c7c68816aef3
Mansur Ziiatdinov <gltronred@gmail.com>
2022-03-04 16:22:08 +0300

    Solution

--- 2-sets/L2.java
+++ 2-sets/L2.java
@@ -4,10 +4,15 @@ public class L2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        Set<Integer> olds = new HashSet<>();
         Integer x = 0;
         do {
             x = sc.nextInt();
             // был ли x введён ранее?
+            if (olds.contains(x)) {
+                System.out.println("Was earlier: " + x);
+            }
+            olds.add(x);
         } while (x != 0);
     }
 }
