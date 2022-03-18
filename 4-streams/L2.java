fa70e967e6bd5276b10ec9305678993693e66094
Mansur Ziiatdinov <gltronred@gmail.com>
2022-03-18 16:07:17 +0300

    Решение

--- 4-streams/L2.java
+++ 4-streams/L2.java
@@ -2,27 +2,6 @@ import java.util.*;
 import java.util.function.*;
 import java.util.stream.*;
 
-class Div2 implements Function<Integer,Integer> {
-    public Integer apply(Integer x) {
-        return x / 2;
-    }
-}
-
-class Add3 implements Function<Integer,Integer> {
-    public Integer apply(Integer x) {
-        return x + 3;
-    }
-}
-
-class Out implements Consumer<Integer> {
-    public void accept(Integer n) {
-        for (int i=0; i<n; i++) {
-            System.out.print("a");
-        }
-        System.out.println();
-    }
-}
-
 public class L2 {
     static void task2(List<Integer> list) {
         list.stream()
@@ -31,9 +10,24 @@ public class L2 {
                         return x % 2 == 0;
                     }
                 })
-            .map(new Div2())
-            .map(new Add3())
-            .forEach(new Out());
+            .map(new Function<Integer,Integer>() {
+                    public Integer apply(Integer x) {
+                        return x / 2;
+                    }
+                })
+            .map(new Function<Integer,Integer>() {
+                    public Integer apply(Integer x) {
+                        return x + 3;
+                    }
+                })
+            .forEach(new Consumer<Integer>() {
+                    public void accept(Integer n) {
+                        for (int i=0; i<n; i++) {
+                            System.out.print("a");
+                        }
+                        System.out.println();
+                    }
+                });
     }
     public static void main(String[] args){
         List<Integer> l = new LinkedList<>();
