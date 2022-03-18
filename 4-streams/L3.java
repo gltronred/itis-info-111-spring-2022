e121d8c7b02df46945adfa1e16edc3d80f4dbb76
Mansur Ziiatdinov <gltronred@gmail.com>
2022-03-18 16:17:19 +0300

    Лямбды

---
+++ 4-streams/L3.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.util.function.*;
+import java.util.stream.*;
+
+public class L3 {
+    static void task2(List<Integer> list) {
+        list.stream()
+            .filter((Integer x) -> {
+                        return x % 2 == 0;
+                    })
+            .map(new Function<Integer,Integer>() {
+                    public Integer apply(Integer x) {
+                        return x / 2;
+                    }
+                })
+            .map(new Function<Integer,Integer>() {
+                    public Integer apply(Integer x) {
+                        return x + 3;
+                    }
+                })
+            .forEach(new Consumer<Integer>() {
+                    public void accept(Integer n) {
+                        for (int i=0; i<n; i++) {
+                            System.out.print("a");
+                        }
+                        System.out.println();
+                    }
+                });
+    }
+    public static void main(String[] args){
+        List<Integer> l = new LinkedList<>();
+        l.add(2);
+        l.add(3);
+        l.add(4);
+        task2(l);
+    }
+}
