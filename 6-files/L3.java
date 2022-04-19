13acff1af2432babac356b2568893d0066ffe78e
Mansur Ziiatdinov <gltronred@gmail.com>
2022-04-19 12:39:25 +0300

    Задание

--- 6-files/L3.java
+++ 6-files/L3.java
@@ -1,8 +1,19 @@
 import java.nio.file.*;
 
+/*
+ * Рекурсивно пройтись по всем файлам в sourceDir,
+ * скопировать их в подпапку targetDir, названную
+ * по расширению файла.
+ *
+ * Если какие-то исходные файлы при этом
+ * перезаписывают результат, вывести сообщение на
+ * экран.
+ */
 public class L3 {
     public static void task(Path sourceDir, Path targetDir) {
-        System.out.println("Move by types from " + sourceDir + " to " + targetDir);
+        System.out.println("Copy by types from " +
+                           sourceDir + " to " +
+                           targetDir);
     }
     public static void main(String[] args) {
         if (args.length != 2) {
