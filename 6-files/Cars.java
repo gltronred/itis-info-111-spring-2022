6b73326f81e2ff7338081adc6d9635dd05e3c88e
Mansur Ziiatdinov <gltronred@gmail.com>
2022-04-26 12:47:09 +0300

    Разделение с учётом ведущих пробелов

--- 6-files/Cars.java
+++ 6-files/Cars.java
@@ -5,11 +5,20 @@ import java.util.*;
 public class Cars {
     public static void task1(Path input) throws IOException {
         Files.lines(input)
-            .map(s -> s.split("\t"))
+            //ср.: (" |\t"))
+            //ср.: ("( |\t)+"))
+            .map(s -> s.split(" *\t *"))
             .map(Arrays::toString)
             .forEach(System.out::println);
     }
+    public static int task2(Path input) throws IOException {
+        return (int)Files.lines(input)
+            .map(s -> s.split("\t"))
+            .filter(arr -> arr[3].equals("Черный"))
+            .count();
+    }
     public static void main(String[] args) throws IOException {
         task1(Path.of("data.txt"));
+        System.out.println(task2(Path.of("data.txt")));
     }
 }
